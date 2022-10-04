ad031aee095ef98a60fe803b3013631d69506a8a
Lubaiba Zakaria <v-lzakaria@traveldoo.com>
2022-10-04 18:07:52 +0530

    [TECH-460][GoogleAPI]Caching

--- src/main/java/com/google/api/model/autoComplete/Place.java
+++ src/main/java/com/google/api/model/autoComplete/Place.java
@@ -2,12 +2,14 @@ package com.google.api.model.autoComplete;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class Place {
+public class Place implements Serializable {
     private String description;
     private String placeId;
 }
--- src/main/java/com/google/api/model/placeId/PlaceResponse.java
+++ src/main/java/com/google/api/model/placeId/PlaceResponse.java
@@ -3,13 +3,15 @@ package com.google.api.model.placeId;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
+import java.io.Serializable;
+
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class PlaceResponse {
+public class PlaceResponse implements Serializable {
 
     private String latitude;
     private String longitude;
